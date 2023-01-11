f73da9dc1a1dcfdc9c3036f0b97164084728140b
a1ecbr0wn <alec@noser.net>
2023-01-11 08:39:59 +0000

    Initial commit

---
+++ template/r53-ddns.rb
@@ -0,0 +1,17 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+class R53-ddns < Formula
+  url "https://github.com/a1ecbr0wn/r53-ddns/archive/refs/tags/:tap_version.tar.gz"
+  version ":tap_version"
+  sha256 ":tap_sha256"
+  desc "Amazon Route 53 DDNS - `r53-ddns` - a way to keep a consistant url for a network where the external ip address may change via Amazon Route 53."
+  homepage "http://r53-ddns.noser.net/"
+  license "Apache 2.0"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+end
+  
