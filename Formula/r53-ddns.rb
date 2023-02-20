464a108dea5ab35520cf54c2b3c79de17aa36ee7
a1ecbr0wn <a1ecbr0wn@users.noreply.github.com>
2023-02-20 22:20:39 +0000

    Update formula to r53-ddns v1.0.2

--- Formula/r53-ddns.rb
+++ Formula/r53-ddns.rb
@@ -3,7 +3,7 @@
 class R53Ddns < Formula
   url "https://github.com/a1ecbr0wn/r53-ddns/archive/refs/tags/v1.0.2.tar.gz"
   version "v1.0.2"
-  sha256 "494f7984e559a453dd33f69d194b6372c1a64e3f33b90bdd6531a1ea5b5d8d97"
+  sha256 "1262919e71197d9183e3c5d5e60be0528b685daf8aa5f5d5dbacedefc6e1845e"
   desc "Amazon Route 53 DDNS - `r53-ddns` - a way to keep a consistant url for a network where the external ip address may change via Amazon Route 53."
   homepage "http://r53-ddns.noser.net/"
   license "Apache 2.0"
