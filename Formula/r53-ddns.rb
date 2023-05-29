abbcc3d8e13d1a6afe06223bdddb9b4dad8ee4b3
a1ecbr0wn <a1ecbr0wn@users.noreply.github.com>
2023-05-29 22:43:31 +0000

    Update formula to r53-ddns v1.0.4

--- Formula/r53-ddns.rb
+++ Formula/r53-ddns.rb
@@ -1,9 +1,9 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 class R53Ddns < Formula
-  url "https://github.com/a1ecbr0wn/r53-ddns/archive/refs/tags/v1.0.3.tar.gz"
-  version "v1.0.3"
-  sha256 "23d7cc9c79d064fa2830119243b034f04f94895321f927c03d2dc7a0ed26330d"
+  url "https://github.com/a1ecbr0wn/r53-ddns/archive/refs/tags/v1.0.4.tar.gz"
+  version "v1.0.4"
+  sha256 "72f753bd94c5a4fb1481cbc4a3f5421a291ff4f39ff1b280e9a4a5a646c447d3"
   desc "Amazon Route 53 DDNS - `r53-ddns` - a way to keep a consistant url for a network where the external ip address may change via Amazon Route 53."
   homepage "http://r53-ddns.noser.net/"
   license "Apache 2.0"
